2713893bf587b797027cc888386119fe85497d67
meigarom <meigaromlopes@gmail.com>
2020-05-01 10:31:19 -0300

    first commit on deploy

---
+++ deploy/handler.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pickle
+from flask import Flask, request
+
+# load model
+model = pickle.load(open('/Users/meigarom/repos/SejaUmDataScientist/deploy/model_wine_quality.pkl', 'rb'))
+
+# instanciate flask
+app = Flask( __name__ )
+
+@app.route( '/predict', methods=['POST'] )  
+def predict():
+    test_json = request.get_json()
+
+    # collect data
+    if test_json:
+        if isinstance( test_json, dict ): # unique value
+            df_raw = pd.DataFrame( test_json, index=[0] )
+        else:
+            df_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+
+    # prediction
+    pred = model.predict( df_raw )
+
+    df_raw['prediction'] = pred
+
+    return df_raw.to_json( orient='records' )
+
+if __name__ == '__main__':
+    # star flask 
+    app.run( host='0.0.0.0', port='5000' )
