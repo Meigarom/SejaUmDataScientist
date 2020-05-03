566ccf02023f36aff0021e1a9b7b4c27c6a7f21a
meigarom <meigaromlopes@gmail.com>
2020-05-03 12:30:28 -0300

    add class with data preparation

---
+++ deploy/wine_quality.py
@@ -0,0 +1,16 @@
+import pickle 
+
+class WineQuality( object ):
+    def __init__( self ):
+        self.free_sulfur_scaler = pickle.load( open( '/Users/meigarom/repos/SejaUmDataScientist/deploy/free_sulfur_scaler.pkl', 'rb'))
+        self.total_sulfur_scaler = pickle.load( open( '/Users/meigarom/repos/SejaUmDataScientist/deploy/total_sulfur_scaler.pkl', 'rb'))
+        
+    
+    def data_preparation( self, df ):
+        # rescaling free sulfur
+        df['free sulfur dioxide'] = self.free_sulfur_scaler.transform( df[['free sulfur dioxide']].values )
+        
+        # rescaling total sulfur
+        df['total sulfur dioxide'] = self.total_sulfur_scaler.transform( df[['total sulfur dioxide']].values )
+        
+        return df
